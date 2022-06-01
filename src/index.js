3edde653faa1055af3add912506e1cfb2fa6ddb5
sexfat <bryant6831@gmail.com>
2022-06-01 13:53:15 +0800

    add

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ function aa(x){
  return x*10 
 }
 console.log(aa(5));
+
+// jquery
+import $ from 'jquery';
+
+import './css/header.css';
+import './css/footer.css';
+import './css/style.css';
+
+
+$('body').css('background-color' , "#010027");
