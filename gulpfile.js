f874e22c2e747a9a8c4a606d9aa1c17c20ee71af
sexfat <bryant6831@gmail.com>
2022-06-01 09:41:48 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -108,6 +108,19 @@ function browser(done) {
     done();
 }
 
+const imagemin = require('gulp-imagemin');
+
+function min_images(){
+    return src('src/images/*.*')
+    .pipe(imagemin([
+        imagemin.mozjpeg({quality: 70, progressive: true}) // 壓縮品質      quality越低 -> 壓縮越大 -> 品質越差 
+    ]))
+    .pipe(dest('dist/images/min'))
+}
+
+
+exports.minify = min_images;
+
 
 exports.default =  series(parallel(sassstyle, includeHTML , miniJs ,mvimages), browser) 
 
